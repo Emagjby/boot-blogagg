19e7ba0d18b1b489304404adc78953c26a49dcc0
Emagjby <emagjby@gmail.com>
2026-03-25 18:38:24 +0200

    chapter 3

--- internal/parser/commands.go
+++ internal/parser/commands.go
@@ -28,7 +28,67 @@ func RegisterCommands() *Commands {
 	cmds.register("reset", CommandReset)
 	cmds.register("users", CommandListUsers)
 	cmds.register("agg", CommandAgg)
+	cmds.register("addfeed", CommandAddFeed)
+	cmds.register("feeds", CommandListFeeds)
 	return cmds
+}
+
+func CommandListFeeds(s *state.State, cmd Command) error {
+	feeds, err := s.Db.ListFeed(context.Background())
+	if err != nil {
+		return fmt.Errorf("Failed to list feeds: %v", err)
+	}
+
+	if len(feeds) == 0 {
+		fmt.Println("No feeds found.")
+		return nil
+	}
+
+	fmt.Println("Feeds:")
+	for _, feed := range feeds {
+		fmt.Printf("- %s: %s (by: %s)\n", feed.Name, feed.Url, feed.Username)
+	}
+
+	return nil
+}
+
+func CommandAddFeed(s *state.State, cmd Command) error {
+	if len(cmd.Args) < 2 {
+		return fmt.Errorf("Usage: gator addfeed <name> <url>")
+	}
+
+	name := cmd.Args[0]
+	url := cmd.Args[1]
+
+	username, err := s.Config.GetUser()
+	if err != nil {
+		return fmt.Errorf("No user is currently logged in. Please login or register first.")
+	}
+
+	user_id, err := s.Db.GetUser(context.Background(), username)
+	if err != nil {
+		return fmt.Errorf("Failed to retrieve user information: %v", err)
+	}
+
+	id := uuid.New()
+	created_at := time.Now()
+	updated_at := time.Now()
+
+	feed := database.CreateFeedParams{
+		ID:        id,
+		CreatedAt: created_at,
+		UpdatedAt: updated_at,
+		Name:      name,
+		Url:       url,
+		UserID:    user_id.ID,
+	}
+
+	if _, err := s.Db.CreateFeed(context.Background(), feed); err != nil {
+		return fmt.Errorf("Failed to create feed: %v", err)
+	}
+
+	fmt.Printf("Feed '%s' added successfully.\n", name)
+	return nil
 }
 
 func CommandAgg(s *state.State, cmd Command) error {
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -15,3 +15,8 @@ INSERT INTO feeds(
     $6
 ) RETURNING *;
 
+
+-- name: ListFeed :many
+SELECT feeds.name, feeds.url, users.name AS username
+FROM feeds
+JOIN users ON feeds.user_id = users.id;
